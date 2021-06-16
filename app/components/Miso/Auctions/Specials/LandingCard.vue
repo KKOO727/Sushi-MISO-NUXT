4fee2dbc6795792938ed64088ea2ef0b4705d759
AristoK3 <85313707+KKOO727@users.noreply.github.com>
2021-06-16 07:53:05 +0200

    main updated.....

--- app/components/Miso/Auctions/Specials/LandingCard.vue
+++ app/components/Miso/Auctions/Specials/LandingCard.vue
@@ -54,8 +54,12 @@
 							{{ raisedTargetLabel }}
 							<nobr>
 								{{ raiseTargetFrom }}
-								{{ textCheck(marketInfo.paymentCurrency.symbol) }}
-								<span v-if="status.type !== 'batch'">~</span>
+								<span v-if="status.type !== 'batch'">
+									{{ textCheck(marketInfo.paymentCurrency.symbol) }}~
+								</span>
+								<span v-else>
+									{{ textCheck(tokenInfo.symbol) }}
+								</span>
 							</nobr>
 							<nobr>
 								{{ raiseTargetTo }}
@@ -177,11 +181,13 @@
 							</span>
 							<span v-if="status.type === 'batch'">
 								{{ totalCommitments }}
-								Tokens Sold
+								{{ textCheck(marketInfo.paymentCurrency.symbol) }}
+								Raised
 							</span>
 							<span v-if="status.type === 'dutch'">
 								{{ totalCommitments }}
 								{{ textCheck(marketInfo.paymentCurrency.symbol) }}
+								Raised
 							</span>
 						</p>
 						<p class="countdown-instance-live text-white">
@@ -357,7 +363,8 @@ export default {
 			return require('~/assets/images/' + this.mobilecardimg)
 		},
 		logoImage() {
-			return this.logoimg
+			if (this.logoimg && this.logoimg.startsWith('http')) return this.logoimg
+			return require('~/assets/images/' + this.logoimg)
 		},
 		seconds: () => 1000,
 		minutes() {
@@ -483,7 +490,7 @@ export default {
 			if (this.status.type === 'crowdsale') {
 				ret = 'Raised Target: '
 			} else if (this.status.type === 'batch') {
-				ret = 'Raised: '
+				ret = 'Total: '
 			} else if (this.status.type === 'dutch') {
 				ret = 'Target: '
 			}
